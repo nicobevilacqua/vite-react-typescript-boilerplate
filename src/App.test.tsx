e32725df9975690207f4222627579cab1069aed0
Nicolas Bevilacqua <nicobevilacqua@gmail.com>
2022-02-07 13:30:03 -0300

    wip

---
+++ src/App.test.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { act } from 'react-dom/test-utils';
+import * as ReactDOM from 'react-dom';
+import App from './App';
+
+describe('App', function () {
+  it('should display pass in number', function () {
+    let container = document.createElement('div');
+    document.body.appendChild(container);
+    act(() => {
+      ReactDOM.render(<App num={191} />, container);
+    });
+    const button = container.querySelector('button') as HTMLButtonElement;
+    expect(button.textContent).toBe(`count is: ${191}`);
+  });
+});
